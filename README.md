40b6e545154d402ef2658169bd22d13b234ed5cb
Valdir Amaral <valdir.amaral58@gmail.com>
2022-07-28 18:19:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+ Meu portfólio básico feito com HTML5, CSS3 com Bulma e JavaScript
---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+function legendahtml() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'HTML: É a linguagem de marcação para páginas web. Tenho conhecimentos básicos, mas continuo aprimorando todos os dias!'
+}
+function legendaexcel() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'Excel: Tenho certificação da Microsoft do uso do Excel (editor de planilhas eletrônicas) e outras ferramentas do Office. Agora sigo estudando para em breve ser capaz de criar dashboards interativos e macros com VBA.'
+}
+function legendacss() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'CSS: É a linguagem de estilo para integrar em arquivos HTML. Tenho noções das propriedades básicas, mas ainda tenho dificuldade em tornar o site responsivo - detalhe: esse portfólio utiliza elementos do framework CSS Bulma.'
+}
+function legendajs() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'JavaScript: É a linguagem de programação utilizada para tornar interfaces mais interativas. Ainda estou engatinhando, mas sei da sintaxe e a utilização de eventos DOM.'
+}
+function legendaingles() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'Língua inglesa: Tenho certificado de inglês intermediário B2, seguindo métricas da EF SET. Consigo ler e escrever textos (às vezes com pequenas dúvidas) e ouvir. Estou em busca de evoluir meu inglês fluentemente em comunicação.'
+}
+function legendaescrita() {
+    var paragrafo = window.document.getElementById("texto-habilidade");
+    paragrafo.innerHTML = 'Redação: Sempre gostei de redigir textos (principamente dissertativo-argumentativos) e tenho facilidade com a gramática portuguesa.'
+}
+
